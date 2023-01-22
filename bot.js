ecf7e4670ce541079375bfa0f942d008ccdc90f8
Bekzodbek <kodirov2000@list.ru>
2023-01-22 15:25:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,9 +6,15 @@ bot.start((ctx) => ctx.reply('Hi 🤖'));
 bot.on('message', async (ctx) => {
     if (ctx.message.location) {
         console.log(ctx.message);
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${ctx.message.location.latitude}&lon=${ctx.message.location.longitude}&appid=d3dd613a1d6d4b5e7f644cd0d78ee34b`;
+
+        const lat = ctx.message.location.latitude;
+        const lon = ctx.message.location.longitude;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d3dd613a1d6d4b5e7f644cd0d78ee34b`;
+
         const response = await axios.get(url);
+
         console.log(response);
+
         ctx.reply(`${response.data.name}: ${response.data.main.temp} C`)
     }
 })
