fa69ae674e076638fbd600dc081a68086063733f
Bekzodbek <kodirov2000@list.ru>
2023-03-02 17:55:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,20 +2,23 @@ const { Telegraf } = require('telegraf');
 const axios = require('axios');
 
 const bot = new Telegraf('5945055423:AAHISEODoL3cWLU8D7VA4UKCQdPunGZro7Q');
+
 bot.start((ctx) => ctx.reply('Hi 🤖'));
+
+
+
+// Getting weather using geolocation
 bot.on('message', async (ctx) => {
     if (ctx.message.location) {
-        console.log(ctx.message);
+        const apiKey = 'd3dd613a1d6d4b5e7f644cd0d78ee34b';
 
-        const lat = ctx.message.location.latitude;
-        const lon = ctx.message.location.longitude;
-        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d3dd613a1d6d4b5e7f644cd0d78ee34b`;
+        const latitude = ctx.message.location.latitude;
+        const longitude = ctx.message.location.longitude;
+        const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
         const response = await axios.get(url);
 
-        console.log(response);
-
-        ctx.reply(`${response.data.name}: ${response.data.main.temp} C`)
+        ctx.reply(`${response.data.name}: ${response.data.main.temp}°С`)
     }
 })
 
